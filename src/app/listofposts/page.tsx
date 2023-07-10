4cf8d22373e236e9f143068dcae7d49bb750e0ce
purupurupu <purupurupu0prog@gmail.com>
2023-07-10 23:48:31 +0900

    get random data

--- src/app/listofposts/page.tsx
+++ src/app/listofposts/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const getPostsData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   return res.json();
@@ -8,11 +10,22 @@ const getUsersData = async () => {
   return res.json();
 };
 
+const getDogData = async () => {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random");
+  return res.json();
+};
+
 export default async function ListOfPosts() {
-  const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
+  const [posts, users, dog] = await Promise.all([
+    getPostsData(),
+    getUsersData(),
+    getDogData(),
+  ]);
   // console.log(users);
   return (
     <div>
+      <h1>List of Posts</h1>
+      <Image src={dog.message} alt="dog" width={500} height={500} />
       {posts.map((post: any) => {
         // return key and title of each post
         return <div key={post.id}>{post.title}</div>;
